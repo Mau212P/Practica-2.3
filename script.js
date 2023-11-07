0056fc8cd33a49cd96202e1083f5be19564d5036
Mau212P <127159342+Mau212P@users.noreply.github.com>
2023-11-06 23:07:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,36 @@ Integrantes:
 - Mauricio Posadas Rodríguez
 - Erick Eduardo Ramos Contreras
 */
+// Espera a que el contenido de la página se cargue antes de ejecutar el código Javascript
+document.addEventListener('DOMContentLoaded', function() {
+  // Obtener referencia a los elementos del DOM
+  const searchInput = document.querySelector('input[name="search"]');
+  const searchForm = document.querySelector('form');
+  const actionSelect = document.querySelector('select[name="action"]');
+  const filterSelect = document.querySelector('select[name="filter"]');
+  const table = document.querySelector('table');
+  const rows = table.querySelectorAll('tbody tr');
+
+  // Función para filtrar por palabra de búsqueda 
+  function filterBySearch(searchTerm) {
+    rows.forEach(row => {
+      const title = row.querySelector('td:nth-child(3)').textContent;
+      if(!title.includes(searchTerm)) {
+        row.style.display = 'none'; 
+      } else {
+        row.style.display = '';
+      }
+    });
+  }
+
+  // Función para filtrar por género
+  function filterByGenre(genre) {
+    rows.forEach(row => {
+      const rowGenre = row.querySelector('td:nth-child(6)').textContent;
+      if(rowGenre !== genre) {
+        row.style.display = 'none';
+      } else {
+        row.style.display = ''; 
+      }
+    });
+  }
